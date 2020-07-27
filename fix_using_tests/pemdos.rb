5c143292354c3fbe273ef997076bd9112b98305e
Jackson Beytebiere <jacksongrowson@gmail.com>
2020-07-27 03:38:45 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,9 @@
 # don't forget to add: require 'pry'
 
 def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
-  else
-    string
+  i = 0
+  while i <= 10
+  "s" + string
+  i + 1
   end
 end
